56dafd6b02be5e2877b5c2293385d106e6bcff84
theanarkh <2923878201@qq.com>
2020-10-23 22:40:22 +0800

    m

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const { Server } = require('./server');
 const { Client } = require('./client');
 const RequestManager = require('./lib/RequestManager');
+const { packet, seq } = require('tiny-application-layer-protocol');
 
 module.exports = {
     Server,
     Client,
     RequestManager,
+    packet,
+    seq,
 }
